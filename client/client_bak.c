b0a8f47e8fb65487a6dcdd055fcd191548768aae
loveskycloud <896275569@qq.com>
2021-04-05 22:23:38 +0800

    solve kill client send empty message

--- client/client_bak.c
+++ client/client_bak.c
@@ -31,6 +31,38 @@ int main()
     msg.flag = 2;
 
     send(sockfd, &msg, sizeof(msg), 0);
+
+    pid_t pid;
+
+
+    if ((pid = fork()) < 0) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        memset((char *)&msg, 0, sizeof(msg));
+        strcpy(msg.from, get_value(conf, (char *)"Client_Name"));
+        msg.flag = 2;
+        fflush(stdin);
+        while (1) {
+            memset(msg.message, 0, sizeof(msg.message));
+            printf("please input :");
+            scanf("%[^\n]s", msg.message);
+            getchar();
+            if (strlen(msg.message) <= 0) {
+                continue;
+            }
+            if (send(sockfd, &msg, sizeof(msg), 0) < 0) {
+                perror("send");
+                exit(1);
+            }
+        }
+    } else {
+        wait(NULL);
+    }
+
+/*
     while (true) {
         printf("please input : ");
         scanf("%[^\n]s", msg.message);
@@ -40,7 +72,7 @@ int main()
             exit(1);
         }
     }
-
+*/
     close(sockfd);
     return 0;
 }
